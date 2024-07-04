68cb260e87adf9c1e0789d479cbb9ce105bbf6f2
Therese Borén <therese.boren@folksam.se>
2024-07-04 16:05:38 +0200

    Adding click function

--- 7Wonders/7Wonders-Leaders.js
+++ 7Wonders/7Wonders-Leaders.js
@@ -1,3 +1,7 @@
-amytis =  document.getElementById("amytis");
+amytis = document.getElementById("Amytis");
+alexander = document.getElementById("Alexander");
+archimedes = document.getElementById("Archimedes");
 
-amytis.addEventListener("click", function(){ alert("From Pic"); });
+amytis.addEventListener("click", function(){ alert("Amytis"); });
+alexander.addEventListener("click", function(){ alert("Alexander"); });
+archimedes.addEventListener("click", function(){ alert("Archimedes"); });
